863c25418c73cad075bdcd0625060ae8615cf994
Kent Nagata <ngtknt@me.com>
2016-01-17 01:38:00 +0900

    add base test

--- spec/yodaspeak_spec.rb
+++ spec/yodaspeak_spec.rb
@@ -5,4 +5,13 @@ describe Yodaspeak do
     expect(Yodaspeak::VERSION).not_to be nil
   end
 
+  describe '#headers' do
+    subject { yodaspeak.instance_eval { headers } }
+    let(:yodaspeak) { Yodaspeak.new("API_KEY") }
+
+    it "gets headers" do
+      is_expected.to include "X-Mashape-Key" => "API_KEY",
+                             "Accept"        => "text/plain"
+    end
+  end
 end
